9dba6c63d43e18cdc996f5749cad9f5258251be5
Roméo Vandoolaeghe <90157318+RomeoVandoolaeghe@users.noreply.github.com>
2023-03-29 00:34:23 +0200

    Add files via upload

--- calculatrice.js
+++ calculatrice.js
@@ -5,11 +5,13 @@ class BaseCalculator {
   }
 
   addToInput(charClicked) {
+    this.MaListe.push(charClicked);
     document.getElementById("input").value += charClicked;
   }
   
 
   clearInput() {
+    this.MaListe.push("clear");
     document.getElementById("input").value = "";
   }
 
@@ -17,12 +19,35 @@ class BaseCalculator {
     var input = document.getElementById("input").value;
     try {
       var result = eval(input);
+      this.MaListe.push("compute");
       document.getElementById("result").value = input + " = " + result;
     } catch {
       document.getElementById("result").value = "Error";
     }
-    clearInput();
+    this.clearInput();
   }
+
+  getList() {
+    return this.MaListe;
+  }
+
+  setInputFromList(liste) {
+    this.clearInput();
+    for (let i = 0; i < liste.length; i++) {
+      if (liste[i] === "clear") {
+        this.clearInput();
+      } else if (liste[i] === "compute") {
+        this.computeResult();
+      } else {
+        this.addToInput(liste[i]);
+      }
+    }
+  }
+
+  clearList() {
+    this.MaListe = [];
+  }
+
 
 
 }
